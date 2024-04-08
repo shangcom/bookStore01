8413c993944017fcf2f17bc9a622332564d6bd92
shangcom <dndbs91@gmail.com>
2024-04-08 10:31:07 +0900

    Swagger의존성 추가, SwaggerConfig 파일 추가. 현재 main/main.do 접속 안됨.

---
+++ src/main/java/com/bookStore01/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.bookStore01.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.bookStore01")) // 컨트롤러 패키지 경로
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("BookStore01 API 문서")
+                .description("BookStore01 프로젝트 API 설명서입니다.")
+                .version("1.0")
+                .build();
+    }
+}
